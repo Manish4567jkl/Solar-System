00a787c9c5f7f3aa88e463dbcd54b00f7c98637c
Manish4567jkl <manishmandal895@gmail.com>
2024-12-27 23:34:50 +0530

    add planets and a bit of rotation

--- src/script.js
+++ src/script.js
@@ -1,16 +1,15 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { color } from "three/tsl";
 import { Pane } from "tweakpane";
 
-// initialize pane
+
 const pane = new Pane();
 
-// initialize the scene
+
 const scene = new THREE.Scene();
 
-// add stuff here
 
-// initialize the camera
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
@@ -20,31 +19,62 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 100;
 camera.position.y = 5;
 
-// initialize the renderer
+const sphereGeometry = new THREE.SphereGeometry(1,32,32)
+const sunMaterial = new THREE.MeshBasicMaterial({
+  color:"#FDE2AF"
+})
+
+
+const sun = new THREE.Mesh(sphereGeometry,sunMaterial)
+sun.scale.setScalar(7)
+scene.add(sun)
+
+const earthMaterial = new THREE.MeshBasicMaterial({
+  color:"blue"
+})
+
+const earth = new THREE.Mesh(sphereGeometry,earthMaterial)
+earth.position.x = 17
+earth.scale.setScalar(3)
+scene.add(earth)
+
+const moonMaterial = new THREE.MeshBasicMaterial({
+  color:'white'
+})
+
+const moon = new THREE.Mesh(sphereGeometry,moonMaterial)
+moon.position.x = 2
+moon.scale.setScalar(0.2)
+earth.add(moon)
+
+
+
 const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// add controls
+
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.maxDistance = 200;
 controls.minDistance = 20
 
-// add resize listener
+
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// render loop
-const renderloop = () => {
+
+const animate = () => {
   controls.update();
+  earth.rotation.y += 0.01;
+  sun.rotation.y += 0.01;
   renderer.render(scene, camera);
-  window.requestAnimationFrame(renderloop);
+  window.requestAnimationFrame(animate);
 };
 
 
-renderloop();
+animate();
